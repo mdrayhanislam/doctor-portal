2d2e660f8d4ff181f654b085b7b3a3d25a2b19b8
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-08-28 12:49:42 +0600

    payment

--- src/Page/Dashboard/Payment.js
+++ src/Page/Dashboard/Payment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery } from 'react-query'
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
 import { loadStripe } from '@stripe/stripe-js'
@@ -19,23 +19,22 @@ const Payment = () => {
         }
     }).then(res => res.json()))
     if (isLoading) {
-        <Loading></Loading>
+      return <Loading></Loading>
     }
     return (
         <div>
-
             <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
                 <div class="card-body">
-                    <p className='text-success font-bold'>Hello, {appointment?.patientName}</p>
-                    <h2 class="card-title">Pay for {appointment?.treatment}</h2>
-                    <p>Your Appointment: <span className='text-orange-700'>{appointment?.date}</span> at {appointment?.slot}</p>
-                    <p>Please pay: ${appointment?.price}</p>
+                    <p className="text-success font-bold">Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your Appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay: ${appointment.price}</p>
                 </div>
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
                     <Elements stripe={stripePromise}>
-                        <CheckoutForm />
+                        <CheckoutForm appointment={appointment} />
                     </Elements>
                 </div>
             </div>
